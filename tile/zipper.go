01e9dc195aa2852b12ed24b0a17a551e5f880204
Darren Bathgate <darren.bathgate@broadcom.com>
2024-04-23 10:32:17 -0400

    added a create zip function that replaces archivex - arhivex was not closing the zip file properly - Resolves: #[35]

--- tile/zipper.go
+++ tile/zipper.go
@@ -1,10 +1,13 @@
 package tile
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
 
-	"github.com/jhoonb/archivex"
 	"github.com/mholt/archiver"
 )
 
@@ -16,19 +19,9 @@ func NewZipper() Zipper {
 
 func (z Zipper) Zip(zipDir, outputFile string) error {
 	zipFile := fmt.Sprintf("%s.zip", outputFile)
-	zf := archivex.ZipFile{}
 
-	err := zf.Create(zipFile)
-	if err != nil {
-		return err
-	}
+	err := z.CreateZip(zipDir, zipFile)
 
-	err = zf.AddAll(zipDir, false)
-	if err != nil {
-		return err
-	}
-
-	err = zf.Close()
 	if err != nil {
 		return err
 	}
@@ -39,6 +32,82 @@ func (z Zipper) Zip(zipDir, outputFile string) error {
 	}
 
 	return nil
+}
+
+func (a Zipper) CreateZip(zipDir string, zipFile string) error {
+
+	_, err := os.Stat(zipDir)
+
+	if err != nil {
+		return err
+	}
+
+	destinationZip, err := os.Create(zipFile)
+
+	if err != nil {
+		return err
+	}
+
+	defer destinationZip.Close()
+
+	zipWriter := zip.NewWriter(destinationZip)
+
+	defer zipWriter.Close()
+
+	return filepath.Walk(zipDir, func(filePath string, info os.FileInfo, err error) error {
+
+		if err != nil {
+			return err
+		}
+
+		if zipDir == filePath {
+			return nil
+		}
+
+		relPath := strings.TrimPrefix(filePath, zipDir+string(os.PathSeparator))
+
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+
+		if err != nil {
+			return err
+		}
+
+		header.Name = relPath
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		zippedFile, err := zipWriter.CreateHeader(header)
+
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			file, err := os.Open(filePath)
+
+			if err != nil {
+				return err
+			}
+
+			defer file.Close()
+
+			_, err = io.Copy(zippedFile, file)
+
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
 }
 
 func (z Zipper) Unzip(zipFile, outputDir string) error {
